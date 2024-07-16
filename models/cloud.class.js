40a7fc6a773fd9d0325953db32f13cc283e1262a
FK <felix_kroth@web.de>
2024-07-16 19:20:00 +0200

    first commit

---
+++ models/cloud.class.js
@@ -0,0 +1,16 @@
+class Cloud extends MovableObject {
+    y = 20;
+    width = 720;
+    height = 300;
+
+    constructor() {
+        super().loadImage('img/5_background/layers/4_clouds/full.png')
+
+        this.x = Math.random() * 500;
+        this.animate();
+    }
+
+    animate() {
+        this.moveLeft();
+    }
+}
